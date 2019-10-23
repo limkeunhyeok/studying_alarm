f21167caf933dbf4eee65b23a5c2d9af9699f4ae
LKH <gorloom6425@naver.com>
2019-10-23 22:15:18 +0900

    studying_alarm v0.0.1

--- venv/Scripts/pip-script.py
+++ venv/Scripts/pip-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\USERPC\PycharmProjects\notice_bot\venv\Scripts\python.exe
+#!C:\Users\USERPC\PycharmProjects\studying_alarm\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
 __requires__ = 'pip==19.0.3'
 import re
